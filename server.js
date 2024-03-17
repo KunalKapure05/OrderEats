4a8ed23efb58560e9c1903adf9e9e3b29124d831
Sarrveshhh <sarrvesharyaa@gmail.com>
2024-03-17 18:00:44 +0530

    Connected server to mongoDb

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+dotenv.config();
+const mongoose = require("mongoose");
 const app = express();
 
+app.use(bodyParser.json());
 
 
-app.listen(3000,()=>{
-    console.log("Listening on port no :",3000);
+
+mongoose.connect(process.env.URL, {
 })
+.then(() => {
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000 + Connected to mongodDB");
+    });
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
