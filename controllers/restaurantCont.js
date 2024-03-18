5697d1d13396145f89fb5fe05078017ca7c4c494
KunalKapure05 <kapurekunal61@gmail.com>
2024-03-18 18:36:58 +0530

    Show Menu of Selected Restaurant functionality done

--- controllers/restaurantCont.js
+++ controllers/restaurantCont.js
@@ -34,7 +34,29 @@ async function searchRestaurant(req, res) {
 }
 
 
+
+async function MenuRes(req, res) {
+    try {
+        const name = req.params.name; 
+        
+        const restaurant = await Restaurant.findOne({ name }).populate('menu')
+
+        if (!restaurant) {
+            return res.status(404).json({ message: "Restaurant not found" });
+        }
+// i have to return the menuietms
+        const menu = restaurant.menu
+
+        return res.status(200).json({ restaurant: restaurant.name, menu });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+
 module.exports = {
     browseRestaurants,
-    searchRestaurant
+    searchRestaurant,
+    MenuRes
 }
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { browseRestaurants, searchRestaurant} = require('../controllers/restaurantCont');
+const { browseRestaurants, searchRestaurant,MenuRes} = require('../controllers/restaurantCont');
 
 
 router.get('/', browseRestaurants);
 router.get('/search/:name', searchRestaurant);
+router.get('/:name/menu',MenuRes)
 
 module.exports=router;
